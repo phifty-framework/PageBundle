216c11f5b1bbc107264ea792d562783ffbee66dc
c9s <yoanlin93@gmail.com>
2014-12-27 20:31:04 +0800

    Refactoring menu classes

--- Model/Page.php
+++ Model/Page.php
@@ -1,16 +1,19 @@
 <?php
 namespace PageBundle\Model;
+use CoreBundle\Linkable;
 
-class Page 
-extends \PageBundle\Model\PageBase
+class Page extends \PageBundle\Model\PageBase implements Linkable
 {
     public function getLink()
     {
         return "/" . join("/", array("page", $this->handle, $this->id, rawurlencode($this->title) ));
     }
 
-    public function getUrl() {
-        return kernel()->getBaseUrl() . $this->getLink();
+    public function getUrl($absolute = false) {
+        if ($absolute) {
+            return kernel()->getBaseUrl() . $this->getLink();
+        }
+        return "/" . join("/", array("page", $this->handle, $this->id, rawurlencode($this->title) ));
     }
 
     public static function byHandle($handle, $lang = null) {
